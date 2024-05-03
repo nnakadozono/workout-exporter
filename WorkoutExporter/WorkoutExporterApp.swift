9fb5b54073a8b072600f933f3dd0960544798d69
Naoyuki Nakadozono <nnakadozono@users.noreply.github.com>
2024-05-04 08:48:55 +0900

    Initial Commit

---
+++ WorkoutExporter/WorkoutExporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutExporterApp.swift
+//  WorkoutExporter
+//
+//  Created by zono on 2024/05/04.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutExporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
